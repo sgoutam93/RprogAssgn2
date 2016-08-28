a4e497f85db4566f640e6b34583435fd64066386
Shovon Goutam <shovon.g.kuet@gmail.com>
2016-08-29 01:57:37 +0200

    Add files via upload
    ##Description
    This script contains two function to cache inverse matrices. This script was created to fulfill the assignment 2 of R programming Coursera course.

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## makeCacheMatrix function make a matrix object to cache the inverse matrix
+## input argument is a matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL #emptied to store inverse for future use
+        set <- function(y){
+                #this function will be used for setting new matrix
+                x <<- y
+                inv <<- NULL
+                message(inv)
+        }
+        get<-function() x #to get the prcossed special matrix to be used together with the cachesolve function
+        setinv<- function(newinv) inv <<-newinv # to set the newly calculated Inverse matrix
+        getinv<-function () inv  #to get the Invers matrix
+        list(set = set, get = get,
+             setinv=setinv,
+             getinv=getinv)
+        
+ # end of makeVector function       
+
+}
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inv<- x$getinv()
+        if(!is.null(inv)){
+                message("pulling the cache data")
+                return (inv)
+        }
+        #if Inv is empty Invers matrix has to be created
+        data <- x$get()
+        inv<-solve(data,...) #creat invers matrix
+        x$setinv(inv) #pushed for cache
+        inv #return the inverse matrix
+}
